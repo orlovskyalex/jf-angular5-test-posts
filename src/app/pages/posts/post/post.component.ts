bfb4020f4bdb24bea7b5c0b4a51c63b8c10060d1
Aleksei Kaida <alex94orlovsky@gmail.com>
2018-10-12 14:06:44 +0300

    implement deleting post

--- src/app/pages/posts/post/post.component.ts
+++ src/app/pages/posts/post/post.component.ts
@@ -1,19 +1,33 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs/Subject';
 import { Post } from '../../../shared/interfaces/post.interface';
+import { PostsService } from '../../../shared/services/posts.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
 })
-export class PostComponent implements OnInit {
+export class PostComponent implements OnDestroy {
 
   @Input() post: Post;
 
-  constructor() {
+  private destroyed$ = new Subject();
+
+  constructor(private posts: PostsService) {
   }
 
-  ngOnInit() {
+  deletePost() {
+    this.posts.deletePost(this.post.id)
+      .pipe(
+        takeUntil(this.destroyed$),
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
   }
 
 }
